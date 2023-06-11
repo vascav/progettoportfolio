335d6d2634d7fb93b65522e22bdb4d5303f1bfc5
Saverio Cavalcante <122820458+vascav@users.noreply.github.com>
2023-06-11 20:06:39 +0200

    Add files via upload

---
+++ assets/js/carosello.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var words = document.getElementsByClassName('word');
+    var currentWordIndex = 0;
+  
+    setInterval(function() {
+      words[currentWordIndex].classList.remove('active');
+      currentWordIndex = (currentWordIndex + 1) % words.length;
+      words[currentWordIndex].classList.add('active');
+    }, 1000);
+  });
+  
