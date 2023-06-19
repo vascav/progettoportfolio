1cbec379a5865fd30623858ade12bb0776b52a0e
vascav <saveriocavalcante@gmail.com>
2023-06-19 11:26:32 +0200

    Aggiunto menu a comparsa

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+});
+
