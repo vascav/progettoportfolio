6767eac03b4d2b37f20e88975d5814a28e978d82
vascav <saveriocavalcante@gmail.com>
2023-07-12 12:34:54 +0200

    Done

--- assets/js/menu.js
+++
@@ -1,7 +0,0 @@
-const apertura = document.querySelector('.apertura');
-const navbar = document.querySelector('.navbar');
-
-apertura.addEventListener('click', () => {
-  navbar.classList.toggle('show');
-});
-
